0f0c993a8e677a414e412e2b8ee09ffab40bbd46
jerry-stan <89541963+jerry-stan@users.noreply.github.com>
2022-03-24 20:10:09 -0600

    Update unemployment_rates_agricluture.sql

--- unemployment_rates_agricluture.sql
+++ unemployment_rates_agricluture.sql
@@ -6,16 +6,16 @@
 
 USE US_employment;
 SELECT year
-,unemployed_percent
-,labor_force AS "labor_force (000s)"
-,unemployed AS "unemployed (000s)"
-,agrictulture_ratio AS Ag_employed
-,CAST(CAST(agrictulture_ratio AS decimal(9,2))/labor_force*100 AS DECIMAL(10,2)) AS percent_agriculture
+	,unemployed_percent
+	,labor_force AS "labor_force (000s)"
+	,unemployed AS "unemployed (000s)"
+	,agrictulture_ratio AS Ag_employed
+	,CAST(CAST(agrictulture_ratio AS decimal(9,2))/labor_force*100 AS DECIMAL(10,2)) AS percent_agriculture
 FROM employment_rates
 WHERE
-unemployed_percent = (SELECT MAX(unemployed_percent) FROM employment_rates)
-OR
-unemployed_percent = (SELECT MIN(unemployed_percent) FROM employment_rates);
+	unemployed_percent = (SELECT MAX(unemployed_percent) FROM employment_rates)
+	OR
+	unemployed_percent = (SELECT MIN(unemployed_percent) FROM employment_rates);
 
 --------------------------------------------------------------------------------
 -- returns years with the highest and lowest agricultural unemployment rates
@@ -39,5 +39,5 @@ SELECT year
 FROM cte
 
 WHERE percent_agriculture = (SELECT MIN(percent_agriculture) FROM cte)
-OR
-percent_agriculture = (SELECT MAX(percent_agriculture) FROM cte)
+	OR
+	percent_agriculture = (SELECT MAX(percent_agriculture) FROM cte)
